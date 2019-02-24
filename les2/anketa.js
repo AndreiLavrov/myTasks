ee656bb132754776e89fbb8e9de1c4d6913e5088
AndreiLavrov <lavrikffk@mail.ru>
2019-02-24 15:19:42 +0100

    changed the name input validation function, used regular expressions

--- les2/anketa.js
+++ les2/anketa.js
@@ -1,28 +1,32 @@
+/*
+Ask user: surname, first name, patronymic, age in years, gender
+and display the user profile.
+*/
 function profile() {
 
     var firstName, lastName, fatherName, dateOfBirth, userAgeYears, userAgeDays, userAgeYearsAfter,
         gender, retiree;
 
-    /*to check for empty line and cancel input*/
+    /*to check for empty line, cancel input, only letters*/
     function validate(res) {
 
-        while (res == null || res === '' || !isNaN(res)) {
-            res = prompt('Incorrect input, enter valid data ');
+        while (res === null || res === '' || res.search(/[^a-zA-Z]/g) >= 0)  {
+            res = prompt('Incorrect input, enter valid data ', 'Name');
         }
 
         return res;
     }
 
-    /*revers and check length date of birth*/
+    /*revers and check length date of birth, for empty line, cancel input*/
     function validDate(str) {
 
         var arr = str.split('.');
 
-        while(isNaN(arr[0] + arr[1] + arr[2]) ||
+        while (str === null || str === '' || isNaN(arr[0] + arr[1] + arr[2]) ||
         arr[2].length > 4 || arr[1].length > 2 || arr[0].length > 2 ||
         arr[0] > 31 ||  arr[1] > 12 || arr[2] > new Date().getFullYear()) {
 
-            arr = prompt('Incorrect input, enter valid data ').split('.');
+            arr = prompt('Incorrect input, enter valid data ', '18.06.1991').split('.');
         }
 
         return arr.reverse();
@@ -56,7 +60,7 @@ function profile() {
     firstName =  validate(prompt('What is your first name?', 'Andrei') );
     lastName = validate(prompt('What is your last name?', 'Laurou') );
     fatherName = validate(prompt('What is your name on the father?', 'Valeryevich') );
-    dateOfBirth = validDate( validate(prompt('What is your date of birth?', '18.06.1991')) );
+    dateOfBirth = validDate(prompt('What is your date of birth?', '18.06.1991'));
 
     getUserAge(dateOfBirth);
 
