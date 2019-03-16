54a097caecfda8127798552928f055ef5c938f1f
AndreiLavrov <lavrikffk@mail.ru>
2019-03-16 19:06:12 +0300

    les7

--- les7/sequence.js
+++ les7/sequence.js
@@ -1,3 +1,11 @@
+
+/*
+counter
+from @start (default 0)
+with step @step (default 1)
+@return number
+*/
+
 function sequence (start, step) {
 		var count = start || 0;
 		var myStep = step || 1;
@@ -11,21 +19,39 @@ function sequence (start, step) {
 		}
 }
 
-// function  partitial(mult, a, b) {
-// 		return mult.bind(null, a, b);
-// };
+ /*multiplication params */
 function mult(a, b, c, d) { return a * b * c * d; }
-
+/* multiplication with fixed params
+(caring)*/
 function  partitial(mult, a, b) {
-		return function () {
-
-					return mult.call(null, arguments);
+		return function myFun(c, d) {
+					return mult.call(null, a, b, c, d);
 		}
-
 };
 
 console.log(mult(2, 3, 4, 5)); // 120
 mult23 = partitial(mult, 2, 3);
 console.log(mult23(4, 5)); //120
+
+
+/*used value of input
+@surname
+and @name
+@return string*/
+const elem = document.getElementById('elem');
+
+function func(surname, name) {
+		return alert(this.value + ', ' + surname + ' ' + name);
+}
+
+		 let obj = {
+				value: elem.value,
+		 }
+
+		 func =  func.bind(obj);
+
+// func('Иванов', 'Иван'); //тут должно вывести 'привет, Иванов Иван'
+// func('Петров', 'Петр'); //тут должно вывести 'привет, Петров Петр'
+
 
 
